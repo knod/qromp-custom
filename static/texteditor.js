dc9edeb52fb52ae3b40de3cfa27acfec6cf478c5
knod <knodding@gmail.com>
2014-03-01 08:24:56 -0500

    More pseudo code, new function placeholders, colors

--- static/texteditor.js
+++ static/texteditor.js
@@ -11,6 +11,10 @@
 var editor = {
 	/* Enclosure for text editor functions */
 
+	// The colors for an activated row
+	activeNumRow: "#dcdcdc",
+	activeTextRow: "#f0f0f0",
+
 	keyFilter: function (key) {
 		/* (int) -> None
 
@@ -25,15 +29,14 @@ var editor = {
 			$(this).outerHeight($(".text-row").outerHeight());
 		});
 
+		// Expands the input textarea size to show all text
 		// $(".text-row").elastic();
 
 		// If the enter key was pressled
 		if (key.keyCode == 13) {
 
-			// When the user presses enter in a textfield in the
-			// visualizer's editor
-			// Run a function in texteditor.js that adds a line
-			// and updates the row numbers
+			// Add a line and updates the row numbers
+
 		}
 
 		// If the delete key was pressed
@@ -48,12 +51,13 @@ var editor = {
 
 	},
 
-	addLine: function (currentLine) {
+	addLine: function (textRow) {
 		/* (element) -> None
 
 		Adds divs below current input, one for the
 		rows, one for the text, then numbers the
-		rows appropriately.
+		rows appropriately. Moves curosr to the new
+		input field
 		*/
 
 		// How do we match up the number row to the
@@ -64,10 +68,31 @@ var editor = {
 
 		// Hmmm, but we have to make it actvie...
 
+		// Create the .num-row div
+		// Create the .text-row input
+		// Store the .num-row as the .text-row's data value
+		// Append the divs to editor using the current .text-row's
+		// .num-row data value to add the new .num-row
+		// Move the cursor to the new .text-row input
+		// Activate the new row
+		// Deactivate this row
+
+		// Renumber the rows
+		editor.updateNums();
+
 	},
 
-	removeLine: function () {
+	removeLine: function (textRow) {
+		/* (element) -> None
 
+		Deletes this input and it's matching .num-row,
+		then numbers the rows appropriately. Moves curosr
+		to the previous input field.
+		*/
+
+		// Move the cursor to the previous input field
+		// Remove the .num-row in this .text-row's data value
+		// 
 	},
 
 	updateNums: function () {
@@ -80,8 +105,27 @@ var editor = {
 		$(".num-row").each(
 			function (thisIndex) {
 				$(this).text(thisIndex + 1);
-			}
-		);
+			});
+	},
 
-	}
+	activateRow: function (textRow) {
+		/* (element) -> None
+
+		Change the colors of textRow to the
+		active colors.
+		*/
+
+		// Change textRow's color
+		// Change color of textRow's numRow data value
+	},
+
+	deactivateRow: function (textRow) {
+		/* (element) -> None
+
+		Remove the colors of textRow.
+		*/
+
+		// Remove textRow's color
+		// Remove color of textRow's numRow data value
+	},
 }
