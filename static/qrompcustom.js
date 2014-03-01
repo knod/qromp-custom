79d0fe422229ef66aeb693e26ea0c1e5853d5acc
knod <knodding@gmail.com>
2014-03-01 07:56:58 -0500

    More notes

--- static/qrompcustom.js
+++ static/qrompcustom.js
@@ -1,5 +1,10 @@
 /* knod 02/28/14
 * The general functions for qromp, quantum computer simulator
+* 
+* Notes:
+* 1. Do we want the bottom line of the enditor to remain
+* blank for the evaluate button
+* 
 */
 
 $(document).ready(function () {
@@ -8,21 +13,30 @@ $(document).ready(function () {
 
 	$(".text-row").keydown(function (key) {
 
+		// Always resize the number row to match the text row
+		// This only matters if we make text areas work
+		// HOW THE HELL DO WE GET RID OF THE LINE AT THE END
 		$(".num-row").outerHeight($(".text-row").outerHeight());
 		// $(".text-row").elastic();
 
+		// If the enter key was pressled
 		if (key.keyCode == 13) {
 
 			// When the user presses enter in a textfield in the
 			// visualizer's editor
 			// Run a function in texteditor.js that adds a line
 			// and updates the row numbers
-	
+		}
+
+		// If the delete key was pressed
+		else if (false){
 			// When the user presses delete in an empty textfield
 			// Run a function in texteditor.js that removes a
 			// line and updates the row numbers
 		}
 
+		// If the up arrow key was pressed
+		// If the down arrow key was pressed
 	})
 
 	// Testing
